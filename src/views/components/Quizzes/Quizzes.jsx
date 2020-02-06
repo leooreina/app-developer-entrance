64973f2c6d369448d5034465ccd6ce4d7ded08fc
Leonardo Reina <leonardo@formahomolog.com.br>
2020-02-06 19:34:13 -0300

    WIP mostrando primeira tela

--- src/views/components/Quizzes/Quizzes.jsx
+++ src/views/components/Quizzes/Quizzes.jsx
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
-import * as json from '../../../data/quiz.json'
+import React, { Component } from "react";
+import * as json from "../../../data/quiz.json";
 
 export default class Quizzes extends Component {
   state = {
     data: []
-  }
-  
+  };
+
   componentDidMount() {
-    this.transformData(json)
+    this.transformData(json);
   }
 
   transformData(json) {
@@ -17,21 +17,26 @@ export default class Quizzes extends Component {
     let newData = this.state.data;
     this.setState({
       data: newData
-    })
+    });
   }
 
   render() {
-    let { data } = this.state
+    let { data } = this.state;
     return (
       <div>
-        {
-        data ? data.map(iten => {
-          iten.default.quizzes.map(quiz => {
-            console.log(quiz.title)
-          })
-        }): null 
-        }
+        <h1 className="text-center mt-5">Start</h1>
+        {data
+          ? data.map(iten =>
+              iten.default.quizzes.map(quiz => (
+                <div className="my-4 text-center" key={quiz.title}>
+                  <button className="btn-info border-0">
+                    <h2>{quiz.title}</h2>
+                  </button>
+                </div>
+              ))
+            )
+          : null}
       </div>
-    )
+    );
   }
-} 
+}
