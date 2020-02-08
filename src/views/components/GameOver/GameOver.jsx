bef45b114fdd7457ac3bf9e9498070e1ca79849f
leooreina <leo.reina01@gmail.com>
2020-02-08 14:33:43 -0300

    adding feature fail/pass

--- src/views/components/GameOver/GameOver.jsx
+++ src/views/components/GameOver/GameOver.jsx
@@ -5,10 +5,14 @@ export const GameOver = (props) => {
   return (
     <div className="text-center my-4">
       <h2 className="game-over" key="game-over">GAME OVER!</h2>
-      <h4 className="final-score" key="final-score">Final Score: 
+      <h4 className="final-score font-weight-bold" key="final-score">Final Score: 
         <div className="right-questions-number px-3 py-3" key="right-questions-number">
           {props.score} / {props.properties.questions.length}
         </div>
+        {props.score >= 2 
+          ? <div className="pass mt-4">Pass!</div>
+          : <div className="fail mt-4">Fail!</div>
+        }
       </h4>
     </div>
   )
