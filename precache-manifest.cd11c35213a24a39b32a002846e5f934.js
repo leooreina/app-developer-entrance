3ae1974bcb145d02e24f57e391ebbdb48f0e0b17
leooreina <leo.reina01@gmail.com>
2020-02-08 14:37:54 -0300

    Updates

---
+++ precache-manifest.cd11c35213a24a39b32a002846e5f934.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96b50cf5d6b1f7b0425bb1d6beadb532",
+    "url": "/app-developer-entrance/index.html"
+  },
+  {
+    "revision": "971df9cdbc0a2e662e76",
+    "url": "/app-developer-entrance/static/css/main.e95acb7c.chunk.css"
+  },
+  {
+    "revision": "a7aa16aea28f26706ef2",
+    "url": "/app-developer-entrance/static/js/2.1d91d107.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/app-developer-entrance/static/js/2.1d91d107.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "971df9cdbc0a2e662e76",
+    "url": "/app-developer-entrance/static/js/main.fa3b03af.chunk.js"
+  },
+  {
+    "revision": "4c96994b40d53d3bd613",
+    "url": "/app-developer-entrance/static/js/runtime-main.3765b656.js"
+  }
+]);
